4c7ffa080064bbf36cddd3236a22c2c691517194
Rafael Savvides <savvidesrafael@gmail.com>
2019-12-05 23:43:09 +0200

    Added danske data

--- R/load_data.R
+++ R/load_data.R
@@ -14,6 +14,22 @@ read_spankki_all <- function(dir_file = readLines("data/dir_bank.txt")) {
   map_df(file_list, read_data)
 }
 
+#' Title
+#'
+#' @param dir_file
+#'
+#' @return
+#' @export
+#'
+#' @examples
+read_all <- function(dir_file = readLines("data/dir_bank.txt")) {
+  file_list = list.files(dir_file)
+  map_df(file_list, read_data) %>%
+    arrange(date) %>%
+    mutate(is_income = amount > 0,
+           is_internal_payment = str_to_lower(payer) %in% c("rafael savvides", "savvides rafael") & str_to_lower(receiver) %in% c("rafael savvides", "savvides rafael"))
+}
+
 #' Read csv into a data frame
 #'
 #' @param filename
@@ -23,7 +39,6 @@ read_spankki_all <- function(dir_file = readLines("data/dir_bank.txt")) {
 #'
 #' @examples
 read_data <- function(filename, dir_file = readLines("data/dir_bank.txt")) {
-  # S-pankki
   #filename = "spankki_20180101_20181231.csv"
   filename = paste0(dir_file, "\\", filename)
   if (grepl("spankki", filename)) {
@@ -59,14 +74,28 @@ read_spankki <- function(filename) {
   book %>%
     mutate(amount = make_numeric(amount),
            date_reported = day_month_year2date(date_reported),
-           date = day_month_year2date(date_paid),
-           is_income = amount > 0,
-           is_internal_payment = str_to_lower(payer) %in% c("rafael savvides", "savvides rafael") & str_to_lower(receiver) %in% c("rafael savvides", "savvides rafael")) %>%
-    dplyr::select(date, amount, type, payer, receiver, message, is_income, is_internal_payment)
+           date = day_month_year2date(date_paid)) %>%
+    dplyr::select(date, amount, type, payer, receiver, message)
 }
 
+#' Read Danske bank data
+#'
+#' @param filename
+#'
+#' @return
+#' @export
+#'
+#' @examples
 read_danske <- function(filename) {
-  #TODO
+  day_month_year2date <- function(x) as.Date(as.character(x), tryFormats = c("%d.%m.%Y"))
+  read.csv2(filename,
+            col.names = c("date", "receiver_payer", "amount", "balance", "status", "check"),
+            stringsAsFactors = FALSE) %>%
+  mutate(payer = if_else(amount > 0, as.character(receiver_payer), "Rafael Savvides"),
+         receiver = if_else(amount < 0, as.character(receiver_payer), "Rafael Savvides"),
+         date = day_month_year2date(date)) %>%
+    select(date, receiver, payer, amount) %>%
+    mutate(receiver = str_replace(receiver, " +\\)\\)\\)\\)", ""))
 }
 
 #' Title
